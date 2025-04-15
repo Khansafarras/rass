b333e7053559076bb83991616a3b2d91f8e6dd91
Khansafarras <2400018190@webmail.uad.ac.id>
2025-04-15 14:55:25 +0700

    Add files via upload

---
+++ belajar git.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string Nama;
+    int tanggal_lahir;
+    string alamat;
+    
+    cout<<"Masukan Nama: ";
+    cin>>Nama;
+    cout<<"Masukan Tanggal Lahir: ";
+    cin>>tanggal_lahir;
+    cout<<"Masukan alamat: ";
+    cin>>alamat;
+    
+    return 0;
